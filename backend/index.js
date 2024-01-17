e7c55682d701d63d6eb19f1e2b0ba10bb20b5ad4
Hope-Have-Happy <3h0925@gmail.com>
2024-01-17 04:28:30 -0500

    Update index.js: add API /sendMessage

--- backend/index.js
+++ backend/index.js
@@ -21,22 +21,45 @@ function initializeOpenAI(apiKey) {
   openai = new OpenAI(apiKey);
 }
 
-app.post("/sendMessage", async (req, res) => {
-  const { messageContent, fileData } = req.body;
-  console.log("Message Recieved", messageContent);
-  const message = await openai.beta.threads.messages.create(currentThread.id, {
-    role: "user",
-    content: messageContent,
-    // fileData: fileData, // Optional file upload handling
-  });
+app.post("/setKey", (req, res) => {
+  let apiKey = req.body.apiKey;
+  console.log(`ApiKey Received: ${apiKey}`);
+  try {
+    initializeOpenAI({ apiKey: apiKey }); // Pass apiKey directly
+    res.status(201).send("API Key Set Successfully");
+    console.log("API Key Set Successfully");
+  } catch (error) {
+    console.error("Error initializing OpenAI client:", error);
+    res.status(500).send("Failed to initialize OpenAI client");
+    return;
+  }
+});
 
-  const run = await openai.beta.threads.runs.create(currentThread.id, {
-    assistant_id: assistantId,
-    instructions:
-      req.body.additionalInstructions || "Provide detailed and rich responses",
-  });
+app.post("/startConversation", async (req, res) => {
+  if (!openai) {
+    res
+      .status(400)
+      .send("OpenAI client is not initialized. Set API key first.");
+    return;
+  }
 
-  res.send({ runId: run.id });
+  try {
+    const assistant = await openai.beta.assistants.create({
+      name: "User Custom Assistant",
+      instructions: req.body.instructions || "Respond to user queries",
+      model: "gpt-3.5-turbo-1106", // This can be dynamic based on user choice
+      tools: [{ type: "code_interpreter" }, { type: "retrieval" }], // DALL·E tool support to be added when available
+    });
+    console.log(`Assistant Id : ${assistant.id}`);
+    assistantId = assistant.id;
+    currentThread = await openai.beta.threads.create();
+
+    res.send({ assistantId: assistant.id, threadId: currentThread.id });
+    console.log(`Thread ID : ${currentThread.id}`);
+  } catch (error) {
+    console.error("Error in startConversation:", error);
+    res.status(500).send("Error starting conversation");
+  }
 });
 
 app.get("/getResponse/:runId", async (req, res) => {
